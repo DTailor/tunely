bdf2dac4a8302603107b4aff2d69302088adcc29
DTailor <dann.croitoru@gmail.com>
2013-07-23 00:16:00 +0300

    adds sample local_settings.py file

---
+++ tunely/sample_local_settings.py
@@ -0,0 +1,29 @@
+ADMINS = (
+    ('Admin', ''),
+)
+ 
+DEBUG = True
+TEMPLATE_DEBUG = True
+COMPRESS_ENABLED = False
+ 
+GMAIL_USER = ""
+GMAIL_PSWD = ""
+MAIL_RECIPIENT = ''
+ 
+SC_CLIENT_ID = ''
+SC_CLIENT_SECRET = ''
+SC_USERNAME = ''
+SC_PSWD = ''
+ 
+DATABASES = {
+    'default': {
+        'ENGINE': '',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+ 
+SECRET_KEY = ''
