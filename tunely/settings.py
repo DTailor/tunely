2b0b4a3678bd81edb3b5b70e7a3c92b8d58ff9ed
DTailor <dann.croitoru@gmail.com>
2013-07-22 22:26:05 +0300

    removes unneeded db settings

--- tunely/settings.py
+++ tunely/settings.py
@@ -22,8 +22,8 @@ ADMINS = (
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db/data.db', # Or path to database file if using sqlite3.
+        'ENGINE': '', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '', # Or path to database file if using sqlite3.
         'USER': '',
         'PASSWORD': '',
         'HOST': '', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
