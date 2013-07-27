aae369690119f3ec631d1e9f27609a0c87b384e9
DTailor <dann.croitoru@gmail.com>
2013-07-27 22:50:35 +0300

    tests for tune_gazer -> root page + station access

--- tune_gazer/tests.py
+++ tune_gazer/tests.py
@@ -1,16 +1,25 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import Client
+from django.core.urlresolvers import reverse
+from tune_gazer.models import *
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TuneTest(TestCase):
+    fixtures = ['test_fixture.json']
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_access_main_page(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_access_individual_station_ok(self):
+        station = Station.objects.all().order_by('?')[0]
+        response = self.client.get('/%s' % station.slug)
+        self.assertEqual(response.status_code, 301)
+
+    def test_access_individual_station_err(self):
+        response = self.client.get('/non-existent-station')
+        self.assertEqual(response.status_code, 301)
